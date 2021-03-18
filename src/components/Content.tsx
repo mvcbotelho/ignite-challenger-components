c1f88f53265204d7dda3d931e831e829b1e7dec3
Marcus Botelho <marcus.botelho@dxtcps004203.local>
2021-03-18 01:22:36 -0300

    Create Content and Sidebar components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+import { GenreResponseProps } from "../interfaces/GenreResponseProps";
+import { MovieProps } from "../interfaces/MovieProps";
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
---
+++ src/interfaces/MovieProps.ts
@@ -0,0 +1,9 @@
+export interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
